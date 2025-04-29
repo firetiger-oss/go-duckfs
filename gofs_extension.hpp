36ce8374b7562d55bd1515d2d92d401a167940c1
Achille Roussel <achille.roussel@gmail.com>
2025-04-29 11:18:55 -0700

    fix double-include
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- gofs_extension.hpp
+++ gofs_extension.hpp
@@ -4,19 +4,18 @@
 #include <duckdb.hpp>
 
 namespace duckdb {
-
   class GoFSExtension : public Extension {
   public:
     void Load(DuckDB &db) override;
     std::string Name() override;
     std::string Version() const override;
   };
-
 }
 
 extern "C" {
-#endif // __cplusplus
+#else
 #include <duckdb.h>
+#endif // __cplusplus
 
 duckdb_state duckfs_register_subsystem(duckdb_database database, int id);
 duckdb_state duckfs_unregister_subsystem(duckdb_database database);
