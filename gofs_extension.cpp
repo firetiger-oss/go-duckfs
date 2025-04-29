66f81f26f0e447b5d7baa7931a8ae72be5c829fb
Achille Roussel <achille.roussel@gmail.com>
2025-04-29 11:38:49 -0700

    only include the capi header
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- gofs_extension.cpp
+++ gofs_extension.cpp
@@ -4,8 +4,8 @@
 #ifndef DUCKDB_AMALGAMATION
 #include <duckdb/common/exception.hpp>
 #include <duckdb/common/string_util.hpp>
-#endif
 #include <duckdb/main/extension_util.hpp>
+#endif
 #include <duckdb/main/capi/capi_internal.hpp>
 #include <gofs_extension.hpp>
 
