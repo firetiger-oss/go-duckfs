1b42568b3e5b8aa29e7a01b241617a18b72d02a2
Achille Roussel <achille.roussel@gmail.com>
2025-04-29 11:36:28 -0700

    add headers missing from the build distribution
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- gofs_extension.cpp
+++ gofs_extension.cpp
@@ -4,9 +4,9 @@
 #ifndef DUCKDB_AMALGAMATION
 #include <duckdb/common/exception.hpp>
 #include <duckdb/common/string_util.hpp>
+#endif
 #include <duckdb/main/extension_util.hpp>
 #include <duckdb/main/capi/capi_internal.hpp>
-#endif
 #include <gofs_extension.hpp>
 
 extern "C" {
